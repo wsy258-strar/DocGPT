cd7b749be070fd86fae5c3de8949f1ab9920196a
Shunyi Wang <3228451861@qq.com>
2024-02-19 14:50:08 +0800

    updata commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv, find_dotenv
 import streamlit as st
 from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter,RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
@@ -9,57 +9,72 @@ from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 import openai
 import os
+from tool.llm import LlmEngine
 
 #API_KEY
 def get_openai_key():
     _ = load_dotenv(find_dotenv())
     return os.environ['OPENAI_API_KEY']
-openai.api_key = get_openai_key()
+
+
+def get_pdf_text(pdf):
+  pdf_reader = PdfReader(pdf)
+  text = ""
+  for page in pdf_reader.pages:
+    text += page.extract_text()
+  return text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        # chunk_size=768,
+        chunk_overlap=200,
+        separators=["\n\n", "\n", " ", ""],
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+# 保存
+def save_vector_store(textChunks):
+    db = FAISS.from_texts(textChunks, OpenAIEmbeddings())
+    db.save_local('faiss')
+ 
+ 
+# 加载
+def load_vector_store():
+    return FAISS.load_local('faiss', OpenAIEmbeddings())
+
+def st_first():
+    st.set_page_config(page_title="Ask your PDF")
+    st.header("Ask your PDF 💬")
+
 
 
 def main():
+  st_first()
+  # upload file
+  pdf = st.file_uploader("Upload your PDF", type="pdf")
+  openai.api_key = get_openai_key()
+  print(openai.api_key)
+  if pdf is not None:
+    text = get_pdf_text(pdf)
+    chunks = get_text_chunks(text)
+    embeddings = OpenAIEmbeddings()
+    knowledge_base = FAISS.from_texts(chunks, embeddings)
 
-    # print(openai.api_key)
-    # load_dotenv()
-    st.set_page_config(page_title="Ask your PDF")
-    st.header("Ask your PDF 💬")
-    
-    # upload file
-    pdf = st.file_uploader("Upload your PDF", type="pdf")
-    
-    # extract the text
-    if pdf is not None:
-      pdf_reader = PdfReader(pdf)
-      text = ""
-      for page in pdf_reader.pages:
-        text += page.extract_text()
-        
-      # split into chunks
-      text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-      )
-      chunks = text_splitter.split_text(text)
-      
-      # create embeddings
-      embeddings = OpenAIEmbeddings()
-      knowledge_base = FAISS.from_texts(chunks, embeddings)
-      
-      # show user input
-      user_question = st.text_input("Ask a question about your PDF:")
-      if user_question:
+    user_question = st.text_input("Ask a question about your PDF:")
+    if user_question:
+
         docs = knowledge_base.similarity_search(user_question)
-        
-        llm = OpenAI(
-                     model_name = "gpt-3.5-turbo-1106")
-        chain = load_qa_chain(llm, chain_type="stuff")
+        llm = LlmEngine()
+        chain = llm.get_qa_chain(knowledge_base)
         with get_openai_callback() as cb:
-          response = chain.run(input_documents=docs, question=user_question)
-          print(cb)
-           
-        st.write(response)
+           response = chain({"query": user_question})
+           print(cb)
+            
+        st.write(response['result'])
+
     
 
 if __name__ == '__main__':
