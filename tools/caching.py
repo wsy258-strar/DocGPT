3f508414d3be6cc6da078e0aac5271cbe65a62c2
Shunyi Wang <3228451861@qq.com>
2024-02-24 17:45:33 +0800

    Updata cashing

---
+++ tools/caching.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from streamlit.runtime.caching.hashing import HashFuncsDict
+
+import tools.parsing as parsing
+import tools.chunking as chunking
+import tools.embedding as embedding
+from tools.parsing import File
+# from tools.llm import LlmEngine
+
+
+def file_hash_func(file: File) -> str:
+    """Get a unique hash for a file"""
+    return file.id
+
+
+@st.cache_data(show_spinner=False)
+def bootstrap_caching():
+    """Patch module functions with caching"""
+
+    # Get all substypes of File from module
+    file_subtypes = [
+        cls
+        for cls in vars(parsing).values()
+        if isinstance(cls, type) and issubclass(cls, File) and cls != File
+    ]
+    file_hash_funcs: HashFuncsDict = {cls: file_hash_func for cls in file_subtypes}
+
+    parsing.read_file = st.cache_data(show_spinner=False)(parsing.read_file)
+    chunking.chunk_file = st.cache_data(show_spinner=False, hash_funcs=file_hash_funcs)(
+        chunking.chunk_file
+    )
+    embedding.embed_files = st.cache_data(
+        show_spinner=False, hash_funcs=file_hash_funcs
+    )(embedding.embed_files)
