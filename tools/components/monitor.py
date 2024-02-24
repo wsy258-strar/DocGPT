07ed73300a30dd444c87530cc883bd5b7f5c61d4
Shunyi Wang <3228451861@qq.com>
2024-02-24 17:47:04 +0800

    Monitor the usage of community version keys

---
+++ tools/components/monitor.py
@@ -0,0 +1,19 @@
+import datetime
+
+def community_tokens_available_pct(used):
+	# used = used
+	print("used:",used)
+	limit = 100000
+	pct = (100.0 * (limit-used) / limit) if limit else 0
+	pct = max(0, pct)
+	pct = min(100, pct)
+	print(pct)
+	return pct
+
+
+def community_tokens_refresh_in():
+	x = datetime.datetime.now()
+	dt = (x.replace(hour=23, minute=59, second=59) - x).seconds
+	h = dt // 3600
+	m = dt  % 3600 // 60
+	return f"{h} h {m} min"
